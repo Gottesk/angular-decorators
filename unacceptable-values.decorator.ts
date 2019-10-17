2766d5a1b60f4a9d0af6be807c8ce16cea3f564f
Dmitriy Lopukhov <87dl@mail.ru>
2019-10-17 22:23:07 +0200

    Update unacceptable-values.decorator.ts

--- unacceptable-values.decorator.ts
+++ unacceptable-values.decorator.ts
@@ -1,29 +1,51 @@
 export function ReturnOnUnacceptableValues(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    const metadataKey = `__unacceptable_${propertyKey}_values`;
-    const originalMethod = descriptor.value;
-    descriptor.value = function (...args) {
-        if (target[metadataKey]) {
-            const unacceptableValues = target[metadataKey];
-            for (let index = 0; index < args.length; index++) {
-                const arg = args[index];
-                if (unacceptableValues[index].includes(arg)) {
-                    return;
-                }
-
-            }
+  const metadataKey = `__unacceptable_${propertyKey}_values`;
+  const originalMethod = descriptor.value;
+  descriptor.value = function (...args) {
+    if (target[metadataKey]) {
+      const unacceptableValues = target[metadataKey];
+      const argIndexes = Object.keys(target[metadataKey]);
+      for (const iterator of argIndexes) {
+        if (unacceptableValues[iterator].includes(args[iterator])) {
+          return;
         }
-        const result = originalMethod.apply(this, args);
-        return result;
-    };
-    return descriptor;
+      }
+    }
+    const result = originalMethod.apply(this, args);
+    return result;
+  };
+  return descriptor;
 }
 
 export function UnacceptableValue<T>(...values: T[]) {
-    return function (target: Object, key: string, index: number) {
-        const metadataKey = `__unacceptable_${key}_values`;
-        if (!target[metadataKey]) {
-            target[metadataKey] = {};
-        }
-        target[metadataKey][index] = values;
-    };
+  return function (target: Object, key: string, index: number) {
+    const metadataKey = `__unacceptable_${key}_values`;
+    if (!target[metadataKey]) {
+      target[metadataKey] = {};
+    }
+    target[metadataKey][index] = values;
+  };
+}
+
+
+
+// usage example 
+class AppComponent {
+  title = 'hello';
+  title2;
+
+  constructor() {
+    this.getTitle(this.title)
+    this.getTitle(undefined);
+    this.getTitle(this.title2);
+    this.getTitle();
+  }
+
+  @ReturnOnUnacceptableValues
+  getTitle(@UnacceptableValue(undefined, null, '') title?: string): string {
+    console.log('Function output: ' + title);
+    return title;
+  }
+  // ==> Function output: hello
+  // in other cases getTitle will return undefined
 }
