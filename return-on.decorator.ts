e2e5a8eb4ac11a1807f5b8973e0c26fa9dd66d74
Dmitriy Lopukhov <87dl@mail.ru>
2019-10-17 22:54:44 +0200

    Update return-on.decorator.ts
    added usage example

--- return-on.decorator.ts
+++ return-on.decorator.ts
@@ -12,3 +12,25 @@ export function ReturnOn<T>(value: T): MethodDecorator {
         return descriptor;
     };
 }
+
+
+
+// usage example 
+class AppComponent {
+
+  constructor() {
+    this.foo(undefined, { key: "foo" });
+    this.foo([1, 2, 3], undefined);
+    this.foo([1, 2, 3], { key: "foo" });
+    this.foo(); // ==> No! function should not have optional params! Use unacceptable-values.decorator.ts for such cases
+  }
+
+  @ReturnOn(undefined)
+  foo(data: number[], bar: { key: string }): void {
+    console.log("Array length: " + data.length);
+    console.log("Object value: " + bar.key);
+  }
+
+  // ==> Array length: 3
+  // ==> Object value: foo
+}
