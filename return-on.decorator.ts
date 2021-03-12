1f17862ead4f22eb3cdb69d43daf84406e128175
Dmitriy Lopukhov <87dl@mail.ru>
2021-03-12 20:43:11 +0100

    formatting

--- return-on.decorator.ts
+++ return-on.decorator.ts
@@ -1,16 +1,20 @@
-export function ReturnOn<T>(value: T): MethodDecorator {
-    return function (target: Function, propertyKey: string, descriptor: PropertyDescriptor) {
-        const originalMethod = descriptor.value;
-        descriptor.value = function (...args) {
-            const hasValue = !!(args && args.filter(i => i === value).length);
-            if (hasValue) {
-                return;
-            }
-            const result = originalMethod.apply(this, args);
-            return result;
-        };
-        return descriptor;
+export function ReturnOn<T>(input: T) {
+  return function <K>(
+    target: K,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: unknown[]) {
+      const hasValue = !!(args && args.filter((i) => i === input).length);
+      if (hasValue) {
+        return;
+      }
+      const result = originalMethod.apply(this, args);
+      return result;
     };
+    return descriptor;
+  };
 }
 
 
