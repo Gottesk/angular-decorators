1772f725c6c3cc3272a03d4eeb17f19a654b3aa2
Dmitriy Lopukhov <87dl@mail.ru>
2021-03-12 20:33:04 +0100

    formatting

--- debounce.decorator.ts
+++ debounce.decorator.ts
@@ -1,25 +1,29 @@
 /**
  * Delays and runs the original method in given milliseconds.
- * 
+ *
  * Could be useful for the keyup events to delay execution until the user has stopped typing for a predetermined amount of time.
  */
-export function Debounce(milliseconds: number): MethodDecorator {
-    return function (target: Function, propertyKey: string, descriptor: PropertyDescriptor) {
-        const map = new WeakMap();
-        const originalMethod = descriptor.value;
-        descriptor.value = function (...params) {
-            let debounced = map.get(this);
-            if (debounced) {
-                clearTimeout(debounced);
-            }
-            debounced = setTimeout(() => {
-                originalMethod.apply(this, params);
-                map.delete(this);
-            }, milliseconds);
-            map.set(this, debounced);
-        };
-        return descriptor;
+export function Debounce(milliseconds: number = 200) {
+  return function <T>(
+    target: T,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const map = new WeakMap();
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...params: unknown[]) {
+      let debounced = map.get(this);
+      if (debounced) {
+        clearTimeout(debounced);
+      }
+      debounced = setTimeout(() => {
+        originalMethod.apply(this, params);
+        map.delete(this);
+      }, milliseconds);
+      map.set(this, debounced);
     };
+    return descriptor;
+  };
 }
 
 
