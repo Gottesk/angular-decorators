f2d16a88a7461bcf2ba7716e47c298a1dd65ef3f
Dmitriy Lopukhov <87dl@mail.ru>
2021-03-12 20:36:33 +0100

    formatting

--- try-catch.decorator.ts
+++ try-catch.decorator.ts
@@ -1,17 +1,19 @@
-export function TryCatch(errorMessage: string): MethodDecorator {
-    return function (target: Function, key: string, descriptor: any) {
-        const originalMethod = descriptor.value;
-        descriptor.value = function (...args: any[]) {
-            try {
-                const result = originalMethod.apply(this, args);
-                return result;
-            } catch (error) {
-                if (errorMessage) {
-                    console.error(errorMessage);
-                }
-                console.error(`A problem happened in ${key} method. The error is ${error}`);
-            }
-        };
-        return descriptor;
+export function TryCatch(errorMessage: string = '') {
+  return function <T>(target: T, key: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: unknown[]) {
+      try {
+        const result = originalMethod.apply(this, args);
+        return result;
+      } catch (error) {
+        if (errorMessage) {
+          console.error(errorMessage);
+        }
+        console.error(
+          `A problem happened in ${key} method. The error is ${error}`
+        );
+      }
     };
+    return descriptor;
+  };
 }
